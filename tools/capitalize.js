1eaceb19794d2e888f75e5d8551675ec33ba9125
ts_ci <ts_ci@github.com>
2021-03-06 16:22:03 +0000

    Enabling shorter import paths [automatic]

---
+++ tools/capitalize.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.capitalize = void 0;
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+exports.capitalize = capitalize;
+//# sourceMappingURL=capitalize.js.map
