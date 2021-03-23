ac6f9bc1e810b8561ee635ce989c677b5cc29c64
William Thiery <williamalexthiery@hotmail.fr>
2021-03-23 23:56:08 +0100

    update

--- docs/useGlobalState.tsx
+++ docs/useGlobalState.tsx
@@ -6,7 +6,9 @@ import { createUseGlobalState } from "powerhooks/useGlobalState";
 
 const { useIsDarkModeEnabled } = createUseGlobalState({
   "name": "isDarkModeEnabled",
-  "defaultValue": () => window.matchMedia("(prefers-color-scheme: dark)").matches,
+  "defaultValue": () => window.matchMedia(
+    "(prefers-color-scheme: dark)"
+    ).matches,
   "persistance": "localStorage"
 });
 
@@ -16,11 +18,16 @@ import { useIsDarkModeEnabled } from "./useIsDarkModeEnabled";
 
 export function MyComponent() {
 
-  const { isDarkModeEnabled, setIsDarkModeEnabled } = useIsDarkModeEnabled();
+  const { 
+    isDarkModeEnabled, 
+    setIsDarkModeEnabled 
+  } = useIsDarkModeEnabled();
 
   return (
     <div>
-      <p>The dark mode is currently: {isDarkModeEnabled?"enabled":"disabled"}</p>
+      <p>
+        The dark mode is currently: {isDarkModeEnabled?"enabled":"disabled"}
+      </p>
       <button onClick={()=> setIsDarkModeEnabled(!isDarkModeEnabled)}>
         Click to toggle dark mode
       </button>
