038162f1e61206ad9ef4d3cbcd2db6c8a6311551
William Thiery <williamalexthiery@hotmail.fr>
2021-04-29 22:29:29 +0200

    new version with useClickAway

---
+++ src/useClickAway.ts
@@ -0,0 +1,26 @@
+import { useRef } from "react";
+import { useEvt } from "evt/hooks";
+import { Evt } from "evt";
+import { useConstCallback } from "./useConstCallback";
+
+
+export function useClickAway<T extends HTMLElement = any>(onClickAway: () => void) {
+
+    const rootRef = useRef<T>(null);
+
+    const onClickAway_const = useConstCallback(onClickAway);
+
+    useEvt(
+        ctx =>
+            Evt.from(ctx, document, "mousedown")
+                .attach(
+                    ({ target }) => !rootRef.current?.contains(target as any),
+                    onClickAway_const
+                ),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    );
+
+    return { rootRef };
+
+}
