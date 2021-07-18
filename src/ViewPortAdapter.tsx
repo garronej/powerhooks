822d284bcac15ef018851a9e217ca0402378d76c
garronej <joseph.garrone.gj@gmail.com>
2021-07-18 05:22:12 +0200

    Fix the ViewPortAdapter API

--- src/ViewPortAdapter.tsx
+++ src/ViewPortAdapter.tsx
@@ -7,30 +7,38 @@ import { useWindowInnerSize } from "./tools/useWindowInnerSize";
 import { useBrowserFontSizeFactor } from "./tools/useBrowserFontSizeFactor";
 import { useEffect, createContext, useContext } from "react";
 
-export type ViewPortConfig = {
-	targetWindowInnerWidth: number;
-	targetBrowserFontSizeFactor: number;
-};
 
-export function matchViewPortConfig(viewPortConfig: ViewPortConfig | ReactNode): viewPortConfig is ViewPortConfig {
-	return (
-		viewPortConfig instanceof Object &&
-		"targetWindowInnerWidth" in viewPortConfig
-	);
+export class ViewPortOutOfRangeError extends Error {
+	constructor(
+		public readonly fallbackNode: ReactNode
+	) {
+
+		super();
+
+		Object.setPrototypeOf(this, new.target.prototype);
+
+	}
 }
 
 export type ViewPortAdapterProps = {
+	/** 
+	 * May throw import { ViewPortOutOfRangeError } from "powerhooks/ViewPortAdapter";
+	 * to specify a fallback screen on edge cases.
+	 */
 	getConfig(
 		props: {
 			windowInnerWidth: number;
 			windowInnerHeight: number;
 			browserFontSizeFactor: number;
-		 }
-	): ViewPortConfig | ReactNode;
+		}
+	): {
+		targetWindowInnerWidth: number;
+		targetBrowserFontSizeFactor: number;
+	};
 	children: ReactNode;
 };
 
-export type ViewPortState = ViewPortConfig & {
+export type ViewPortState = ReturnType<ViewPortAdapterProps["getConfig"]> & {
 	targetWindowInnerHeight: number;
 	zoomFactor: number;
 };
@@ -41,6 +49,29 @@ export function useViewPortState() {
 	const viewPortState = useContext(context);
 	return { viewPortState };
 }
+
+export function getIsPortraitOrientation(props: {
+    windowInnerWidth: number;
+    windowInnerHeight: number;
+
+}) {
+    const { windowInnerWidth, windowInnerHeight } = props;
+
+	const isPortraitOrientation = 
+		windowInnerWidth * 1.3 < windowInnerHeight;
+
+    return isPortraitOrientation;
+}
+
+export const chromeFontSizesFactors = {
+    "Very small": 0.5625,
+    "Small": 0.75,
+    "Medium (Recommended)": 1,
+    "Large": 1.25,
+    "Very Large": 1.5,
+} as const;
+
+export type ChromeFontSize = keyof typeof chromeFontSizesFactors;
 
 /**
  * WARNING: We assumes that html element font-size is not defined
@@ -57,17 +88,19 @@ export function ViewPortAdapter(props: ViewPortAdapterProps) {
 
 	const { browserFontSizeFactor } = useBrowserFontSizeFactor();
 
+	const { getConfigResult } = (function useClosure() {
 
-	const { viewPortState } = (function useClosure() {
-
-		const viewPortStateRef = useRef<ViewPortState | ReactNode>();
+		const getConfigResultRef = useRef<
+			{ isOutOfRange: false, viewPortState: ViewPortState; } |
+			{ isOutOfRange: true, fallbackNode: ReactNode; }
+		>();
 
 		useGuaranteedMemo(
 			() => {
 
 				//We skip refresh when pinch and zoom
 				if (
-					viewPortStateRef.current !== undefined &&
+					getConfigResultRef.current !== undefined &&
 					(
 						window.scrollY !== 0 ||
 						window.scrollX !== 0
@@ -76,19 +109,34 @@ export function ViewPortAdapter(props: ViewPortAdapterProps) {
 					return;
 				}
 
-				const viewPortConfig = getConfig({
-					windowInnerWidth,
-					windowInnerHeight,
-					browserFontSizeFactor
-				});
+				let viewPortConfig: ReturnType<typeof getConfig>;
 
-				if (!matchViewPortConfig(viewPortConfig)) {
+				try {
 
-					viewPortStateRef.current = viewPortConfig;
+
+					viewPortConfig = getConfig({
+						windowInnerWidth,
+						windowInnerHeight,
+						browserFontSizeFactor
+					});
+
+				} catch (error) {
+
+					if (!(error instanceof ViewPortOutOfRangeError)) {
+						throw error;
+					}
+
+					const { fallbackNode } = error;
+
+					getConfigResultRef.current = {
+						"isOutOfRange": true,
+						fallbackNode
+					};
 
 					return;
 
 				}
+
 
 				const zoomFactor = windowInnerWidth / viewPortConfig.targetWindowInnerWidth;
 
@@ -98,7 +146,7 @@ export function ViewPortAdapter(props: ViewPortAdapterProps) {
 					"targetWindowInnerHeight": windowInnerHeight / zoomFactor
 				};
 
-				viewPortStateRef.current = viewPortState;
+				getConfigResultRef.current = { "isOutOfRange": false, viewPortState };
 
 			},
 			[
@@ -109,16 +157,18 @@ export function ViewPortAdapter(props: ViewPortAdapterProps) {
 			]
 		);
 
-		return { "viewPortState": viewPortStateRef.current! };
+		return { "getConfigResult": getConfigResultRef.current! };
 
 	})();
 
 	useEffect(
 		() => {
 
-			if (!matchViewPortConfig(viewPortState)) {
+			if (getConfigResult.isOutOfRange) {
 				return;
 			}
+
+			const { viewPortState } = getConfigResult;
 
 			//NOTE: We assert the font size is defined in percent 
 			//or not defined. We have no way to check it so we make
@@ -146,34 +196,38 @@ export function ViewPortAdapter(props: ViewPortAdapterProps) {
 		},
 		[
 			browserFontSizeFactor,
-			matchViewPortConfig(viewPortState) ?
-				viewPortState.targetBrowserFontSizeFactor :
-				Object
+			getConfigResult.isOutOfRange ?
+				Object : getConfigResult.viewPortState.targetBrowserFontSizeFactor
 		]
 	);
 
+	if (getConfigResult.isOutOfRange) {
+		const { fallbackNode } = getConfigResult;
+		return <>{fallbackNode}</>;
+	}
+
+	const { viewPortState } = getConfigResult;
+
 	return (
-		!matchViewPortConfig(viewPortState) ?
-			<>{viewPortState}</> :
+		<div
+			about={`powerhooks ${ViewPortAdapter.name} outer wrapper`}
+			style={{ "height": "100vh", "overflow": "hidden" }}
+		>
 			<div
-				about={`powerhooks ${ViewPortAdapter.name} outer wrapper`}
-				style={{ "height": "100vh", "overflow": "hidden" }}
+				about={`powerhooks ${ViewPortAdapter.name} inner wrapper`}
+				style={{
+					"transform": `scale(${viewPortState.zoomFactor})`,
+					"transformOrigin": "0 0",
+					"width": viewPortState.targetWindowInnerWidth,
+					"height": viewPortState.targetWindowInnerHeight,
+					"overflow": "hidden"
+				}}
 			>
-				<div
-					about={`powerhooks ${ViewPortAdapter.name} inner wrapper`}
-					style={{
-						"transform": `scale(${viewPortState.zoomFactor})`,
-						"transformOrigin": "0 0",
-						"width": viewPortState.targetWindowInnerWidth,
-						"height": viewPortState.targetWindowInnerHeight,
-						"overflow": "hidden"
-					}}
-				>
-					<context.Provider value={viewPortState}>
-						{children}
-					</context.Provider>
-				</div>
+				<context.Provider value={viewPortState}>
+					{children}
+				</context.Provider>
 			</div>
+		</div>
 	);
 
 }
