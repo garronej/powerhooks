b7bee2d0b20d2afee5e817ac0286a47ec472ae01
garronej <joseph.garrone.gj@gmail.com>
2022-05-23 00:06:52 +0200

    Working proof of concept persistance global state

--- src/test/ssr/pages/index.tsx
+++ src/test/ssr/pages/index.tsx
@@ -1,14 +1,20 @@
-import { useDarkMode } from "next-dark-mode";
+//import { useDarkMode } from "next-dark-mode";
+import { useDarkMode } from "./next-dark-mode";
+import { useFoo } from "./persistanceByCookie";
 
 export default function Index() {
 
     const { darkModeActive, switchToDarkMode, switchToLightMode } = useDarkMode();
 
+    const { foo, setFoo } = useFoo();
+
     return (
-    <>
+        <>
             <h1>Hello World, darkModeActive: {darkModeActive ? "yes" : "false"}</h1>
-            <button onClick={()=> (darkModeActive ? switchToLightMode : switchToDarkMode)() }>Switch dark mode</button>
-    </>
+            <h1>foo: {foo}</h1>
+            <button onClick={() => (darkModeActive ? switchToLightMode : switchToDarkMode)()}>Switch dark mode</button>
+            <button onClick={() => setFoo(`${foo} +1`)}>change foo</button>
+        </>
     );
 
 }
