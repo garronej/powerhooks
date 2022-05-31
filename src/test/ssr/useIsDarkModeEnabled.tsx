e599aec1c1aefeae4c6de726f4d34e59546a5eb7
garronej <joseph.garrone.gj@gmail.com>
2022-06-01 00:18:11 +0200

    Perfect useDarkModeEnabled SSR

--- src/test/ssr/useIsDarkModeEnabled.tsx
+++ src/test/ssr/useIsDarkModeEnabled.tsx
@@ -1,34 +1,65 @@
 /* eslint-disable react/display-name */
-
-import { memo } from "react";
+import { useEffect } from "react";
 import { createUseSsrGlobalState } from "powerhooks/useSsrGlobalState";
+import { updateSearchBarUrl, retrieveParamFromUrl } from "powerhooks/tools/urlSearchParams";
+import Head from "next/head";
 
 
-export const { evtIsDarkModeEnabled, useIsDarkModeEnabled, withIsDarkModeEnabled } = createUseSsrGlobalState({
+export const { useIsDarkModeEnabled, withIsDarkModeEnabled } = createUseSsrGlobalState({
 	"name": "isDarkModeEnabled",
-	"getInitialValueClientSide": () =>
-		window.matchMedia &&
-		window.matchMedia("(prefers-color-scheme: dark)").matches,
+	"getValueSeverSide": appContext => {
+
+		const { theme } = appContext.router.query;
+
+		if (typeof theme !== "string") {
+			return undefined;
+		}
+
+		switch (theme) {
+			case "dark": return { "value": true };
+			case "light": return { "value": false };
+		}
+
+		return undefined;
+
+	},
 	"getInitialValueServerSide": () => ({
 		"doFallbackToGetInitialValueClientSide": true,
 		"initialValue": false
 	}),
-});
+	"getInitialValueClientSide": () =>
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches,
+	"Head": ({ isDarkModeEnabled }) => {
 
-export const DarkModeRootStyle = memo(() => {
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		useEffect(() => {
 
-	const { isDarkModeEnabled } = useIsDarkModeEnabled();
+			const result = retrieveParamFromUrl({
+				"url": window.location.href,
+				"name": "theme"
 
-	return (
-		<style>
-			{`
+			});
+
+			if (!result.wasPresent) {
+				return;
+			}
+
+			updateSearchBarUrl(result.newUrl);
+
+		}, []);
+
+
+		return (
+			<Head>
+				<style>{`
 				:root {
 					color-scheme: ${isDarkModeEnabled ? "dark" : "light"}
 				}
-			`}
-		</style>
-	);
-
-
+			`}</style>
+			</Head>
+		);
+	}
 });
+
 
