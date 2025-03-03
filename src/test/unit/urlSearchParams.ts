d2c8aa743641f6e612e02d350996c3d2831b7de1
garronej <joseph.garrone@protonmail.com>
2025-03-03 15:01:14 +0100

    Fix multiple bugs in url parser

--- src/test/unit/urlSearchParams.ts
+++ src/test/unit/urlSearchParams.ts
@@ -2,6 +2,8 @@ import { addParamToUrl, retrieveParamFromUrl, retrieveAllParamStartingWithPrefix
 import { assert } from "tsafe/assert";
 import { same } from "evt/tools/inDepth/same";
 
+console.log("START");
+
 
 assert(same(
 	addParamToUrl({
@@ -11,6 +13,50 @@ assert(same(
 	}),
 	{
 		"newUrl": "https://example.com?foo=bar"
+	}
+));
+
+assert(same(
+	addParamToUrl({
+		"url": "https://example.com?",
+		"name": "foo",
+		"value": "bar"
+	}),
+	{
+		"newUrl": "https://example.com?foo=bar"
+	}
+));
+
+assert(same(
+	addParamToUrl({
+		"url": "https://example.com#",
+		"name": "foo",
+		"value": "bar"
+	}),
+	{
+		"newUrl": "https://example.com?foo=bar#"
+	}
+));
+
+assert(same(
+	addParamToUrl({
+		"url": "https://example.com#xx",
+		"name": "foo",
+		"value": "bar"
+	}),
+	{
+		"newUrl": "https://example.com?foo=bar#xx"
+	}
+));
+
+assert(same(
+	addParamToUrl({
+		"url": "https://example.com?p=x#xx",
+		"name": "foo",
+		"value": "bar"
+	}),
+	{
+		"newUrl": "https://example.com?p=x&foo=bar#xx"
 	}
 ));
 
@@ -115,6 +161,170 @@ assert(same(
 		"newUrl": "https://datalab.sspcloud.fr/?palette=ultraviolet",
 		"wasPresent": true,
 		"value": "Eurostat"
+	}
+));
+
+assert(same(
+	retrieveParamFromUrl({
+		"url": "https://datalab.sspcloud.fr/?oidc_1=a&oidc_2=b&oidc=c",
+		"name": "oidc"
+	}),
+	{
+		"newUrl": "https://datalab.sspcloud.fr/?oidc_1=a&oidc_2=b",
+		"wasPresent": true,
+		"value": "c"
+	}
+));
+
+assert(same(
+	retrieveAllParamStartingWithPrefixFromUrl({
+		"url": "https://datalab.sspcloud.fr/?oidc_1=a&oidc_2=b&oidc=c&other=xxx",
+		"prefix": "oidc",
+		"doLeavePrefixInResults": false
+	}),
+	{
+		"newUrl": "https://datalab.sspcloud.fr/?other=xxx",
+		values: {
+			"_1": "a",
+			"_2": "b",
+			"": "c"
+		}
+	}
+));
+
+
+
+
+
+assert(same(
+	addParamToUrl({
+		"url": "https://example.com?foo=notBar#xx",
+		"name": "foo",
+		"value": "bar"
+	}),
+	{
+		"newUrl": "https://example.com?foo=bar#xx"
+	}
+));
+
+assert(same(
+	addParamToUrl({
+		"url": "https://example.com?foo=notBar&baz=xxx#xx",
+		"name": "foo",
+		"value": "bar"
+	}),
+	{
+		"newUrl": "https://example.com?baz=xxx&foo=bar#xx"
+	}
+));
+
+assert(same(
+	retrieveParamFromUrl({
+		"url": "https://www.example.com?foo=bar#xx",
+		"name": "foo"
+	}),
+	{
+		"wasPresent": true,
+		"value": "bar",
+		"newUrl": "https://www.example.com#xx"
+	}
+));
+
+assert(same(
+	retrieveParamFromUrl({
+		"url": "https://www.example.com?foo=bar&baz=xxx#xx",
+		"name": "foo"
+	}),
+	{
+		"wasPresent": true,
+		"value": "bar",
+		"newUrl": "https://www.example.com?baz=xxx#xx"
+	}
+));
+
+assert(same(
+	retrieveParamFromUrl({
+		"url": "https://www.example.com?foo=bar&baz=xxx#xx",
+		"name": "yyy"
+	}),
+	{ "wasPresent": false }
+));
+
+assert(same(
+	retrieveParamFromUrl({
+		"url": "https://www.example.com?foo=bar&baz=xxx#xx",
+		"name": "yyy"
+	}),
+	{ "wasPresent": false }
+));
+
+assert(same(
+	retrieveAllParamStartingWithPrefixFromUrl({
+		"url": "https://www.example.com?xxx_foo=a&xxx_bar=b&baz=c#xx",
+		"prefix": "xxx_",
+		"doLeavePrefixInResults": false
+	}),
+	{
+		"newUrl": "https://www.example.com?baz=c#xx",
+		"values": {
+			"foo": "a",
+			"bar": "b"
+		}
+	}
+));
+
+assert(same(
+	retrieveAllParamStartingWithPrefixFromUrl({
+		"url": "https://www.example.com?xxx_foo=a&xxx_bar=b&baz=c#xx",
+		"prefix": "xxx_",
+		"doLeavePrefixInResults": true
+	}),
+	{
+		"newUrl": "https://www.example.com?baz=c#xx",
+		"values": {
+			"xxx_foo": "a",
+			"xxx_bar": "b"
+		}
+	}
+));
+
+assert(same(
+	retrieveParamFromUrl({
+		"url": "https://datalab.sspcloud.fr/?palette=ultraviolet&title=Eurostat#xx",
+		"name": "title"
+	}),
+	{
+		"newUrl": "https://datalab.sspcloud.fr/?palette=ultraviolet#xx",
+		"wasPresent": true,
+		"value": "Eurostat"
+	}
+));
+
+assert(same(
+	retrieveParamFromUrl({
+		"url": "https://datalab.sspcloud.fr/?oidc_1=a&oidc_2=b&oidc=c#xx",
+		"name": "oidc"
+	}),
+	{
+		"newUrl": "https://datalab.sspcloud.fr/?oidc_1=a&oidc_2=b#xx",
+		"wasPresent": true,
+		"value": "c"
+	}
+));
+
+assert(same(
+	retrieveAllParamStartingWithPrefixFromUrl({
+		"url": "https://datalab.sspcloud.fr/?oidc_1=a&oidc_2=b&oidc=c&other=xxx#xx",
+		"prefix": "oidc",
+		"doLeavePrefixInResults": false
+	}),
+	{
+		"newUrl": "https://datalab.sspcloud.fr/?other=xxx#xx",
+		values: {
+			"_1": "a",
+			"_2": "b",
+			"": "c"
+		}
 	}
 ));
 
--- src/tools/urlSearchParams.ts
+++ src/tools/urlSearchParams.ts
@@ -1,112 +1,183 @@
+import { assert } from "tsafe/assert";
+import { id } from "tsafe/id";
 
-export function addParamToUrl(
-    params: {
-        url: string,
-        name: string,
-        value: string;
-    }
-): { newUrl: string; } {
+export function addParamToUrl(params: {
+  url: string;
+  name: string;
+  value: string;
+}): {
+  newUrl: string;
+} {
+  const { url, name, value } = params;
 
-    const { url, name, value } = params;
+  let newUrl = url;
 
-    let newUrl = url;
-
+  {
     const result = retrieveParamFromUrl({
-        url,
-        name
+      url,
+      name,
     });
 
     if (result.wasPresent) {
-
-        newUrl = result.newUrl;
-
+      newUrl = result.newUrl;
     }
+  }
 
-    newUrl += `${newUrl.includes("?") ? "&" : newUrl.endsWith("?") ? "" : "?"}${name}=${encodeURIComponent(value)}`;
+  let hash: string | undefined = undefined;
 
-    return { newUrl };
+  if (newUrl.includes("#")) {
+    const [p1, p2] = newUrl.split("#");
 
+    assert(p2 !== undefined);
+
+    newUrl = p1;
+
+    hash = p2;
+  }
+
+  newUrl += `${
+    newUrl.includes("?") ? (newUrl.endsWith("?") ? "" : "&") : "?"
+  }${name}=${encodeURIComponent(value)}`;
+
+  if (hash !== undefined) {
+    newUrl += `#${hash}`;
+  }
+
+  return { newUrl };
 }
 
-export function retrieveAllParamStartingWithPrefixFromUrl<Prefix extends string, DoLeave extends boolean>(
-    params: {
-        url: string;
-        prefix: Prefix;
-        doLeavePrefixInResults: DoLeave;
+export function retrieveAllParamStartingWithPrefixFromUrl<
+  Prefix extends string,
+  DoLeave extends boolean
+>(params: {
+  url: string;
+  prefix: Prefix;
+  doLeavePrefixInResults: DoLeave;
+}): {
+  newUrl: string;
+  values: Record<DoLeave extends true ? `${Prefix}${string}` : string, string>;
+} {
+  const { url, prefix, doLeavePrefixInResults } = params;
+
+  const { urlWithoutHash, hash } = (() => {
+    if (url.includes("#")) {
+      const [p1, p2] = url.split("#");
+
+      assert(p2 !== undefined);
+
+      return {
+        urlWithoutHash: p1,
+        hash: p2,
+      };
     }
-): { newUrl: string; values: Record<DoLeave extends true ? `${Prefix}${string}` : string, string>; } {
-
-    const { url, prefix, doLeavePrefixInResults } = params;
-
-    const [baseUrl, locationSearch = ""] = url.split("?");
-
-    const values: Record<string, string> = {};
-
-    const { newLocationSearch } = (() => {
-
-        let newLocationSearch = locationSearch
-            .replace(/^\?/, "")
-            .split("&")
-            .map(part => part.split("=") as [string, string])
-            .filter(([key, value_i]) =>
-                !key.startsWith(prefix) ?
-                    true :
-                    (values[doLeavePrefixInResults ? key : key.substring(prefix.length)] = decodeURIComponent(value_i), false))
-            .map(entry => entry.join("="))
-            .join("&")
-            ;
-
-        newLocationSearch = newLocationSearch === "" ?
-            "" : `?${newLocationSearch}`;
-
-        return { newLocationSearch };
-
-    })();
 
     return {
-        "newUrl": `${baseUrl}${newLocationSearch}`,
-        values
+      urlWithoutHash: url,
+      hash: undefined,
     };
+  })();
 
-}
+  const { urlBeforeSearch, search } = (() => {
+    if (urlWithoutHash.includes("?")) {
+      const [p1, p2] = urlWithoutHash.split("?");
 
-export function retrieveParamFromUrl(
-    params: {
-        url: string;
-        name: string;
+      assert(p2 !== undefined);
+
+      return {
+        urlBeforeSearch: p1,
+        search: p2,
+      };
     }
-): { wasPresent: false; } | { wasPresent: true; newUrl: string; value: string; } {
 
-    const { url, name } = params;
+    return {
+      urlBeforeSearch: urlWithoutHash,
+      search: undefined,
+    };
+  })();
 
-    const { newUrl, values } = retrieveAllParamStartingWithPrefixFromUrl({
-        url,
-        "prefix": name,
-        "doLeavePrefixInResults": true
-    });
+  if (search === undefined) {
+    return {
+      newUrl: url,
+      values: id<Record<string, string>>({}),
+    };
+  }
 
-    return name in values ?
-        {
-            "wasPresent": true, newUrl,
-            "value": values[name]
-        } :
-        {
-            "wasPresent": false
-        };
+  const values: Record<string, string> = {};
 
+  const { newLocationSearch } = (() => {
+    let newLocationSearch = search
+      .split("&")
+      .filter((part) => part !== "")
+      .map((part) => part.split("=") as [string, string])
+      .filter(([key, value_i]) =>
+        !key.startsWith(prefix)
+          ? true
+          : ((values[
+              doLeavePrefixInResults ? key : key.substring(prefix.length)
+            ] = decodeURIComponent(value_i)),
+            false)
+      )
+      .map((entry) => entry.join("="))
+      .join("&");
+
+    return { newLocationSearch };
+  })();
+
+  return {
+    newUrl: `${urlBeforeSearch}${
+      newLocationSearch === "" ? "" : `?${newLocationSearch}`
+    }${hash === undefined ? "" : `#${hash}`}`,
+    values,
+  };
 }
 
+export function retrieveAllParamFromUrl(params: { url: string }): {
+  newUrl: string;
+  values: Record<string, string>;
+} {
+  return retrieveAllParamStartingWithPrefixFromUrl({
+    url: params.url,
+    prefix: "",
+    doLeavePrefixInResults: true,
+  });
+}
+
+export function retrieveParamFromUrl(params: {
+  url: string;
+  name: string;
+}):
+  | { wasPresent: false }
+  | { wasPresent: true; newUrl: string; value: string } {
+  const { url, name } = params;
+
+  let { newUrl, values } = retrieveAllParamStartingWithPrefixFromUrl({
+    url,
+    prefix: name,
+    doLeavePrefixInResults: true,
+  });
+
+  if (!(name in values)) {
+    return { wasPresent: false };
+  }
+
+  const { [name]: value, ...rest } = values;
+
+  Object.entries(rest).forEach(
+    ([name, value]) =>
+      (newUrl = addParamToUrl({
+        name,
+        url: newUrl,
+        value,
+      }).newUrl)
+  );
+
+  return {
+    wasPresent: true,
+    newUrl,
+    value: value,
+  };
+}
 
 export function updateSearchBarUrl(url: string) {
-
-    window.history.replaceState(
-        "",
-        "",
-        url
-    );
-
+  window.history.replaceState("", "", url);
 }
-
-
-
-
