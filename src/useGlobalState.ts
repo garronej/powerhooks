ea5c8a6f56f2ced938707728665f5af0a6390a85
garronej <joseph.garrone@protonmail.com>
2025-03-15 04:52:26 +0100

    Rework useGlobalState

--- src/useGlobalState.ts
+++ src/useGlobalState.ts
@@ -1,5 +1,3 @@
-
-import { createContext } from "react";
 import { useConstCallback } from "./useConstCallback";
 import { overwriteReadonlyProp } from "tsafe/lab/overwriteReadonlyProp";
 import type { UseNamedStateReturnType } from "./useNamedState";
@@ -13,43 +11,35 @@ import { updateSearchBarUrl } from "./tools/updateSearchBar";
 
 export type { StatefulObservable };
 
-const {
-    globalStates,
-    persistedGlobalStateNames
-} = (() => {
+const GLOBAL_CONTEXT_KEY = "__powerhooks.useGlobalState.globalContext";
 
-    type SharedContext = {
-        globalStates: Readonly<Record<string, unknown>>;
-        persistedGlobalStateNames: Set<string>;
-    };
-
-    const propertyKey = "__powerhooks_useGlobalState_context";
-
-    const peerDepObj: Record<typeof propertyKey, SharedContext | undefined> = createContext as any;
-
-    let sharedContext = peerDepObj.__powerhooks_useGlobalState_context;
-
-    if (sharedContext === undefined) {
-
-        sharedContext = {
-            "globalStates": {},
-            "persistedGlobalStateNames": new Set<string>()
+declare global {
+    interface Window {
+        [GLOBAL_CONTEXT_KEY]: {
+            initialLocationHref: string;
+            globalStates: Readonly<Record<string, unknown>>;
+            persistedGlobalStateNames: Set<string>;
+            useGlobalStateWrapperByName: Record<string, Record<string, unknown> | undefined>;
+            getUnparsedStatesFromUrlSearchParams_result:
+                | { unparsedStates: Record<string, string> }
+                | undefined;
         };
-
-        Object.defineProperty(peerDepObj, propertyKey, {
-            "configurable": false,
-            "enumerable": false,
-            "writable": false,
-            "value": sharedContext
-        });
     }
+}
 
-    return sharedContext;
+window[GLOBAL_CONTEXT_KEY] ??= {
+    initialLocationHref: window.location.href,
+    globalStates: {},
+    persistedGlobalStateNames: new Set<string>(),
+    useGlobalStateWrapperByName: {},
+    getUnparsedStatesFromUrlSearchParams_result: undefined
+};
 
-})();
+const globalContext = window[GLOBAL_CONTEXT_KEY];
+
+const { initialLocationHref, globalStates, persistedGlobalStateNames } = globalContext;
 
 export { globalStates };
-
 
 function stringify(obj: unknown): string {
     return JSON.stringify([obj]);
@@ -62,257 +52,200 @@ function parse<T>(str: string): T {
 const prefix = "powerhooks_useGlobalState_";
 
 const { injectGlobalStatesInSearchParams, getStatesFromUrlSearchParams } = (() => {
-
     /** Returns the modified url */
     function injectGlobalStatesInSearchParams(url: string): string {
-
         let newUrl = url;
 
         Object.keys(globalStates)
             .filter(name => persistedGlobalStateNames.has(name))
-            .forEach(name =>
-                newUrl =
-                addOrUpdateSearchParam({
-                    "url": newUrl,
-                    "name": `${prefix}${name}`,
-                    "value": stringify(globalStates[name]),
-                    "encodeMethod": "encodeURIComponent"
-                })
+            .forEach(
+                name =>
+                    (newUrl = addOrUpdateSearchParam({
+                        url: newUrl,
+                        name: `${prefix}${name}`,
+                        value: stringify(globalStates[name]),
+                        encodeMethod: "encodeURIComponent"
+                    }))
             );
 
         return newUrl;
-
     }
 
-    const getUnparsedStatesFromUrlSearchParams = memoize0(() => {
+    function getUnparsedStatesFromUrlSearchParams() {
+        if (globalContext.getUnparsedStatesFromUrlSearchParams_result !== undefined) {
+            return globalContext.getUnparsedStatesFromUrlSearchParams_result;
+        }
 
         const doLeavePrefixInResults = false;
 
-        const { valueByName: values}= getAllSearchParamsStartingWithPrefix({
-            "url": window.location.href,
+        const { valueByName: values } = getAllSearchParamsStartingWithPrefix({
+            url: initialLocationHref,
             prefix,
-            doLeavePrefixInResults,
+            doLeavePrefixInResults
         });
 
-        remove_query_params_from_url: {
+        if (Object.keys(values).length !== 0) {
+            // NOTE: We have to re-extract here since the href might have been updated
+            // while we were waiting for the timeout to be executed.
+            const { url_withoutTheParams } = getAllSearchParamsStartingWithPrefix({
+                url: window.location.href,
+                prefix,
+                doLeavePrefixInResults
+            });
 
-            if (Object.keys(values).length === 0) {
-                break remove_query_params_from_url;
-            }
-
-            //NOTE: We use a timeout in case there is multiple instances of 
-            // powerhooks.
-            setTimeout(() => {
-
-                // NOTE: We have to re-extract here since the href might have been updated
-                // while we were waiting for the timeout to be executed.
-                const { url_withoutTheParams } = getAllSearchParamsStartingWithPrefix({
-                    "url": window.location.href,
-                    prefix,
-                    doLeavePrefixInResults,
-                });
-
-                updateSearchBarUrl(url_withoutTheParams);
-
-            }, 0);
-
+            updateSearchBarUrl(url_withoutTheParams);
         }
 
-        return { "unparsedStates": values };
+        return (globalContext.getUnparsedStatesFromUrlSearchParams_result = {
+            unparsedStates: values
+        });
+    }
 
-    })
-
-    function getStatesFromUrlSearchParams<T>(
-        params: {
-            name: string;
-        }
-    ): { wasPresent: false; } | { wasPresent: true; state: T; } {
-
+    function getStatesFromUrlSearchParams<T>(params: {
+        name: string;
+    }): { wasPresent: false } | { wasPresent: true; state: T } {
         const { name } = params;
 
         const { unparsedStates } = getUnparsedStatesFromUrlSearchParams();
 
         if (!(name in unparsedStates)) {
-            return { "wasPresent": false };
+            return { wasPresent: false };
         }
 
         return {
-            "wasPresent": true,
-            "state": parse(unparsedStates[name])
+            wasPresent: true,
+            state: parse(unparsedStates[name])
         };
-
     }
 
     return {
         injectGlobalStatesInSearchParams,
         getStatesFromUrlSearchParams
     };
-
 })();
 
 export { injectGlobalStatesInSearchParams };
 
-
-
-const hotReloadCleanup = new Map<string, () => void>();
-
 /**
- * 
- * Assert: If localStorageKey is not disabled, T must be 
+ *
+ * Assert: If localStorageKey is not disabled, T must be
  * serializable with JSON.stringify.
- * 
+ *
  * const { useFoo } = createUseGlobalState2({
  *     "getDefaultState": ()=> 33,
  *     "name": "foo"
  * });
  *
  * const { foo, setFoo  } = useFoo();
- * 
- * By default persistance is enabled using localStorage.
- * 
+ *
+ * By default persistence is enabled using localStorage.
+ *
  */
-export function createUseGlobalState<T, Name extends string>(
-    params: {
-        name: Name,
-        /** If function called only if not in local storage */
-        initialState: T | (() => T),
-        doPersistAcrossReloads: boolean;
-    }
-): Record<
-    `use${Capitalize<Name>}`,
-    () => UseNamedStateReturnType<T, Name>
-> & Record<
-    `$${Name}`,
-    StatefulObservable<T>
-> {
-
+export function createUseGlobalState<T, Name extends string>(params: {
+    name: Name;
+    /** If function called only if not in local storage */
+    initialState: T | (() => T);
+    doPersistAcrossReloads: boolean;
+}): Record<`use${Capitalize<Name>}`, () => UseNamedStateReturnType<T, Name>> &
+    Record<`$${Name}`, StatefulObservable<T>> {
     const { name, initialState, doPersistAcrossReloads } = params;
 
+    use_cache: {
+        const cache = globalContext.useGlobalStateWrapperByName[name];
 
-    let isActive = true;
+        if (cache === undefined) {
+            break use_cache;
+        }
 
-    {
-        const cleanup = hotReloadCleanup.get(name);
-
-        cleanup?.();
-
-        hotReloadCleanup.set(name, () => { isActive = false; });
+        // @ts-expect-error: We know what we're doing.
+        return cache;
     }
-
 
     if (doPersistAcrossReloads) {
-
         persistedGlobalStateNames.add(name);
-
     }
 
-    //NOTE: We want to clean the url asap so we don't put it in the 
-    // evt getter... 
+    //NOTE: We want to clean the url asap so we don't put it in the
+    // evt getter...
     const urlSearchParam = getStatesFromUrlSearchParams<T>({ name });
 
     const get$xyz = memoize0(() => {
-
         const localStorageKey = `${prefix}${name}`;
 
-        const storeStateInPersistentStorage = !doPersistAcrossReloads ? undefined : (state: T) => {
+        const storeStateInPersistentStorage = !doPersistAcrossReloads
+            ? undefined
+            : (state: T) => {
+                  localStorage.setItem(localStorageKey, stringify(state));
+              };
 
-            if (!isActive) {
-                return;
-            }
+        const $xyz = createStatefulObservable<T>(
+            (() => {
+                const initialValue = (() => {
+                    if (urlSearchParam.wasPresent) {
+                        const { state } = urlSearchParam;
 
-            localStorage.setItem(localStorageKey, stringify(state));
-        };
+                        storeStateInPersistentStorage?.(state);
 
-
-        const $xyz = createStatefulObservable<T>((() => {
-
-            const initialValue = (() => {
-
-                if (urlSearchParam.wasPresent) {
-
-                    const { state } = urlSearchParam;
-
-                    storeStateInPersistentStorage?.(state);
-
-                    return state;
-
-                }
-
-                if (doPersistAcrossReloads) {
-
-                    const serializedState = localStorage.getItem(localStorageKey);
-
-                    if (serializedState !== null) {
-
-                        return parse<T>(serializedState);
-
+                        return state;
                     }
 
-                }
+                    if (doPersistAcrossReloads) {
+                        const serializedState = localStorage.getItem(localStorageKey);
 
-                return typeGuard<() => T>(initialState, typeof initialState === "function") ?
-                    initialState() :
-                    initialState;
+                        if (serializedState !== null) {
+                            return parse<T>(serializedState);
+                        }
+                    }
 
-            })();
+                    return typeGuard<() => T>(initialState, typeof initialState === "function")
+                        ? initialState()
+                        : initialState;
+                })();
 
-            return () => initialValue;
-
-        })());
-
+                return () => initialValue;
+            })()
+        );
 
         if (storeStateInPersistentStorage !== undefined) {
-
             $xyz.subscribe(storeStateInPersistentStorage);
-
         }
 
         return $xyz;
-
     });
 
-
-    Object.defineProperty(
-        globalStates,
-        name,
-        {
-            "enumerable": true,
-            "get": () => get$xyz().current,
-            "configurable": true
-        }
-    );
+    Object.defineProperty(globalStates, name, {
+        enumerable: true,
+        get: () => get$xyz().current,
+        configurable: true
+    });
 
     function useXyz() {
-
         const $xyz = get$xyz();
 
         useRerenderOnChange($xyz);
 
         return {
             [name]: $xyz.current,
-            [`set${capitalize(name)}`]:
-                useConstCallback((setStateAction: T | ((prevState: T) => T)) =>
-                    $xyz.current =
-                    typeGuard<(prevState: T) => T>(setStateAction, typeof setStateAction === "function") ?
-                        setStateAction($xyz.current) :
-                        setStateAction
-                )
+            [`set${capitalize(name)}`]: useConstCallback(
+                (setStateAction: T | ((prevState: T) => T)) =>
+                    ($xyz.current = typeGuard<(prevState: T) => T>(
+                        setStateAction,
+                        typeof setStateAction === "function"
+                    )
+                        ? setStateAction($xyz.current)
+                        : setStateAction)
+            )
         } as any;
-
     }
 
     overwriteReadonlyProp(useXyz as any, "name", `use${capitalize(name)}`);
 
-    return Object.defineProperty(
-        { [useXyz.name]: useXyz } as any,
-        `$${name}`,
-        {
-            "enumerable": true,
-            "get": () => get$xyz()
-        }
-    );
+    const cache = Object.defineProperty({ [useXyz.name]: useXyz } as any, `$${name}`, {
+        enumerable: true,
+        get: () => get$xyz()
+    });
 
+    globalContext.useGlobalStateWrapperByName[name] = cache;
+
+    return cache;
 }
-
-
-
